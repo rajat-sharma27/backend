227365ffb615fe37a5d4d30dc2059e23900266ff
rajat-sharma27 <rajatpndt2703@gmail.com>
2025-08-30 18:23:53 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -6,24 +6,15 @@ dotenv.config({
 })
 
 connectDB()
-
-// import express from "express";
-// import connectDB from "./db";
-// const app=express();
-
-// ( async ()=> {
-//     try{
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error",(error) =>{
-//             console.log("ERROR",error)
-//             throw error
-//         })
-//         app.listen(process.env.PORT, ()=>{
-//             console.log(`App is listening on port ${process.env.PORT}`)
-//         })
-//     }
-//     catch(error){
-//         console.error("ERROR",error)
-//         throw error
-//     }
-// })()
+.then(()=>{
+    app.on((error)=>{
+        console.log("ERROR",error)
+        throw error 
+    })
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`Server is runningat post ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed!! ",err);
+})
---
+++ src/utils/ApiError.js
@@ -0,0 +1,28 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message= "Something went wrong",
+        error=[],
+        stack=""
+
+    ){
+        super(message)
+        this.statusCode=statusCode
+        this.data=null
+        this.message=message
+        this.success=false
+        this.error= errors
+
+        if(stack){
+            this.stack=stack
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+
+  
+    
+}
+
+export {ApiError}
